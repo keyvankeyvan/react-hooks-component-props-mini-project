f45aa7203fd83c22b187d4efbca85363527c1a87
Keyvan Alonso <keyvanalonso@gmail.com>
2022-06-27 17:53:23 -0400

    commit tree e43d2c8122ebe1c1d292f0305ddf79b18c686498

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,13 @@
 import React from "react";
-import ProjectItem from "./ProjectItem"
 
-function About({projects}){
-    const projectList = projects.map((project) => {
-        return (
-            <ProjectItem key={project.id} preview={project.preview} src="https://via.placeholder.com/215"/>
-        )
-    })
-
+function About({about, image = "https://via.placeholder.com/215"}){
     return(
-        <div id="project-list">{projectList}</div>
+        <aside>
+            <img src ={image} alt="blog logo"/>
+            <p>{about}</p>
+        </aside>
     )
 }
+
 
 export default About
--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,17 @@ import Header from "./Header";
 import About from "./About"
 
 import blogData from "../data/blog";
+import ArticleList from "./ArticleList";
 
-//console.log(blogData);
+// SOLUTION AT:
+// https://github.com/keyvankeyvan/react-hooks-component-props-mini-project/tree/solution/src/components
 
 function App() {
   return (
     <div className="App">
       <Header name={blogData.name} />
-      <About projects={blogData.posts} />
+      <About about={blogData.about} image={blogData.image}/>
+      <ArticleList posts={blogData.posts} />
     </div>
   );
 }
